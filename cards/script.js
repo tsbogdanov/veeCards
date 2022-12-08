7d99f985191af540347d6990a4efd0e2d94aca0d
Sina Wolper <sina@thewolpers.com>
2022-12-08 13:13:46 -0800

    Fix viewport on mobile

--- cards/script.js
+++ cards/script.js
@@ -1,11 +1,6 @@
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+const documentHeight = () => {
+  const doc = document.documentElement
+  doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+ }
+ window.addEventListener(‘resize’, documentHeight)
+ documentHeight()
