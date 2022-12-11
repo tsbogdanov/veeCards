4e118ddf017f5d13f7083c90842c17a03506cf2a
Sina <sina@thewolpers.com>
2022-12-10 17:19:50 -0800

    Fix syntax error

--- cards/script.js
+++ cards/script.js
@@ -1,6 +1,7 @@
 const documentHeight = () => {
-  const doc = document.documentElement
-  doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
- }
- window.addEventListener(‘resize’, documentHeight)
- documentHeight()
+	const doc = document.documentElement
+	doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+}
+
+window.addEventListener('resize', documentHeight);
+documentHeight();
